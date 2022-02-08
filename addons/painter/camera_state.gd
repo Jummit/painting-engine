af85ba8671b5ad310085d17228420a75ce14b215
Jummit <jummit@web.de>
2022-02-08 17:59:14 +0100

    Make state of CameraState private

--- addons/painter/camera_state.gd
+++ addons/painter/camera_state.gd
@@ -1,15 +1,21 @@
 extends Reference
 
+"""
+Utility to store all members of a camera relevant to rendering.
+"""
+
 const MEMBERS := ["keep_aspect", "transform", "h_offset", "v_offset", "size",
 		"fov", "near", "far", "projection"]
 
-var state : Dictionary
+var _state : Dictionary
 
 func _init(camera : Camera) -> void:
 	for member in MEMBERS:
-		state[member] = camera[member]
+		_state[member] = camera[member]
 
 
+# Replicate all stored members to the given camera.
 func apply(camera : Camera) -> void:
 	for member in MEMBERS:
-		camera[member] = state[member]
+		camera[member] = _state[member]
+
